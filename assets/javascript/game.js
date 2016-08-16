72ce3be1e76ba6475502c865cec1eb811753faf0
George Ramirez <george.ramirez2@gmail.com>
2016-08-16 15:16:37 -0500

    letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,43 @@ var handball = ['h','a','n','d','b','a','l','l'];
 var canoeing = ['c','a','n','o','e','i','n','g'];
 var dressage = ['d','r','e','s','s','a','g','e'];
 var eventing = ['e','v','e','n','t','i','n','g'];
-var eightDashes = ['_', '_', '_', '_', '_', '_', '_', '_'];
 var sailing = ['s','a','i','l','i','n','g'];
-var sevenDashes = ['_', '_', '_', '_', '_', '_', '_'];
 var trampoline = ['t','r','a','m','p','o','l','i','n','e'];
-var tenDashes = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'];
 var racewalking =['r','a','c','e','w','a','l','k','i','n','g'];
-var elevenDashes = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'];
+var handballLength = handball.length
+var canoeingLength = canoeing.length
+var dressageLength = dressage.length
+var eventingLength = eventing.length
+var sailingLength = sailing.length
+var trampolineLength =trampoline.length
+var racewalkingLength = racewalking.length
+
 
 
 	
 
 // Variables to be added to and placed on screen
-var	tries = 15;
+var	remaining = 15;
 	wins = 0;
 	userGuess = "";
 	correctGuess = "";
 	word = "";
 	wordLength = "";
+	dashesH = "";
+	badChoices = [];/*to be printed on screen as letter already selected*/
+	
+// my attempt at makeing dashes from the length of the word array
+	// makeDashes = function(){
+	// 	for (var i = 0; i < handball.length; i++){
+		
+	// 	(handball[i] = "_" + console.log(makeDashes())
+	// 	}
+	// }
+
+	// makeDashes();
+		
+	
+
 
 
 
@@ -30,33 +49,27 @@ var	tries = 15;
 
 document.onkeyup = function(event) {/*all JS needs to be in this tag*/
 
-	document.querySelector("#hangman").innerHTML = elevenDashes;
+	document.querySelector("#hangman").innerHTML = handball;
 
  var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
 
- 	if(userGuess === 'h'){
- 		console.log("correct");
- 	}
-		 else if(userGuess.indexOf(handball) == -1) {
-		   
-		 console.log("incorrect");
-		} 
+	 for (var i = 0; i < handball.length; i++){
+		
+		if(userGuess == handball[i]){
+				console.log('yes ' + userGuess + ' that letter is in the word');
+				} else {
+					remaining--;console.log(userGuess + " is a wrong choice," + remaining);
+				}	
+}
+			// else {
+			// 		remaining--;console.log(userGuess + " is a wrong choice," + remaining);
+			// 	} --code that works.
+			// else if lose
+			// else if win + next word or end of game
+ 	
 }/*end of document.onkeyup, needs to contain everthing in the JS*/
 
-var dashes8 = function (){
-	document.querySelector("#hangman").innerHTML = elevenDashes;
-}
-
- // if (userGuess == car.letter1 char index of) {
- // 	print letter in that space and start checking for next letter
-
- // }	else if (userGuess !== car.letter1){
- // 	tries--,  document.querySelector(".tries").innerHTML = tries+" more attempts, and then you are eliminated from competion.";
-
- // }	else if (/*this can be where we jump to the next word, or when we end the game*/) {
-
-/*I think the loop will go here based on the number of tries, then go the next set of if/else pointing to the next word.*/ 
-
+// This is the end of my working code and the begining of my notes.
 
 
 
